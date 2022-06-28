e9bea5c7e12203b58cd1a4b8a6f0d347677faa96
alex logorz <alex.logorz@gmail.com>
2022-06-27 22:12:55 -0400

    added link to test ocr and additional todo comments

--- dip.py
+++ dip.py
@@ -2,12 +2,14 @@ import cv2
 
 image = cv2.imread('./screenshots/gold_theme.PNG')
 
+# testing ocr with http://195.148.30.97/cgi-bin/ocr.py
+
 def idfk(image):
     for row in range(0, image.shape[0]):
         for col in range(0, image.shape[1]):
             pixel = image[row,col]
-            # if (220 <= pixel[0] <= 255 and 220 <= pixel[1] <= 255 and 220 <= pixel[2] <= 255) or (0 <= pixel[0] <= 22 and 65 <= pixel[1] <= 153 and 0 <= pixel[2] <= 5) or (11 <= pixel[0] <= 51 and 11 <= pixel[1] <= 51 and 53 <= pixel[2] <= 255):
-            if 16 <= pixel[0] <= 51 and 16 <= pixel[1] <= 51 and 79 <= pixel[2] <= 255:
+            #TODO: for some reason when I add (11 <= pixel[0] <= 51 and 11 <= pixel[1] <= 51 and 53 <= pixel[2] <= 255) to the condition below it doesnt work for the blue text
+            if (220 <= pixel[0] <= 255 and 220 <= pixel[1] <= 255 and 220 <= pixel[2] <= 255) or (0 <= pixel[0] <= 22 and 65 <= pixel[1] <= 153 and 0 <= pixel[2] <= 5):
                 image[row,col] = [0, 0, 0]
             else:
                 image[row,col] = [255, 255, 255]
