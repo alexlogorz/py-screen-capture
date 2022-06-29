ac40e476482b9e55d41d9f84b45eb209f8c414c1
sawyermade <sawyermade0@gmail.com>
2022-06-29 15:59:42 -0400

    lz

--- dip.py
+++ dip.py
@@ -9,7 +9,7 @@ def idfk(image):
         for col in range(0, image.shape[1]):
             pixel = image[row,col]
             #TODO: for some reason when I add (11 <= pixel[0] <= 51 and 11 <= pixel[1] <= 51 and 53 <= pixel[2] <= 255) to the condition below it doesnt work for the blue text
-            if (220 <= pixel[0] <= 255 and 220 <= pixel[1] <= 255 and 220 <= pixel[2] <= 255) or (0 <= pixel[0] <= 22 and 65 <= pixel[1] <= 153 and 0 <= pixel[2] <= 5):
+            if (220 <= pixel[0] <= 255 and 220 <= pixel[1] <= 255 and 220 <= pixel[2] <= 255) or (0 <= pixel[0] <= 22 and 65 <= pixel[1] <= 153 and 0 <= pixel[2] <= 5) or (11 <= pixel[2] <= 51 and 11 <= pixel[1] <= 51 and 53 <= pixel[0] <= 255):
                 image[row,col] = [0, 0, 0]
             else:
                 image[row,col] = [255, 255, 255]
