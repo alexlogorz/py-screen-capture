b61a9830387ad43e9a43f25916d7825e0a2071ac
sawyermade <sawyermade0@gmail.com>
2022-06-29 21:13:03 -0400

    lz

--- ss.py
+++ ss.py
@@ -22,19 +22,19 @@ class ss:
         )
         self.button1.pack()
         self.button_on = False
-
-        self.startingCoords = (0, 0)
-        self.endingCoords = (0, 0)
         self.listener = None
         self.enable_mouse_listener()
-        self.ss_pil = None
-        self.ss_np = None
-        self.today = datetime.date.today().strftime("%d-%m-%Y")
-
         self.reader = easyocr.Reader(['en'], gpu=False)
-        self.results = None
+
+        self.today = datetime.date.today().strftime("%d-%m-%Y")
+        self.startingCoords = (0, 0)
+        self.endingCoords = (0, 0)
         self.w = 0
         self.h = 0
+        
+        self.ss_pil = None
+        self.ss_np = None
+        self.results = None
         
         self.out_dir = 'screenshots'
         if not os.path.exists(self.out_dir):
@@ -44,38 +44,12 @@ class ss:
 
     def take_screenshot(self):
         print('\nTook screenshot, running OCR...')
+
         self.ss_pil = pyautogui.screenshot(region=(self.startingCoords[0], self.startingCoords[1], self.endingCoords[0] - self.startingCoords[0], self.endingCoords[1] - self.startingCoords[1]))
-        # self.ss_pil = pyautogui.screenshot()
-        # self.ss_np = np.array(self.ss_pil)
         self.ss_np = np.array(self.ss_pil)
         self.ss_np = cv2.cvtColor(self.ss_np, cv2.COLOR_RGB2BGR)
         self.h, self.w = self.ss_np.shape[:2]
         self.center = (self.w // 2, self.h // 2)
-        # print(f'image size: {self.h}, {self.w}')
-        # print(f'ss type: {type(ss)}')
-        # self.disable_mouse_listener()
-
-        # ratio_w = 0.09
-        # ratio_h = 0.05
-        # pixel_diff_w = self.w * ratio_w
-        # pixel_diff_h = self.h * ratio_h
-        # left = int(math.floor(self.center[0] - pixel_diff_w))
-        # right = int(math.ceil(self.center[0] + pixel_diff_w))
-        # top = int(math.ceil(pixel_diff_h))
-        # bottom = int(math.floor(self.h - pixel_diff_h))
-        # self.ss_np = self.ss_np[top:bottom, left:right, ...]
-
-        # criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-        # flags = cv2.KMEANS_RANDOM_CENTERS
-        # pv = np.float32(self.ss_np.reshape((-1, 3)))
-        # compactness, labels, centers = cv2.kmeans(pv, 2, None, criteria, 10, flags)
-        # centers = np.uint8(centers)
-        # seg = centers[labels.flatten()].reshape(self.ss_np.shape)
-        # cv2.imwrite('test_seg.png', seg)
-
-        # print(top, bottom, self.ss_np.shape)
-        # cv2.imwrite('test.png', self.ss_np)
-
 
     def get_results(self):
         self.results = self.reader.readtext(self.ss_np, detail=0, paragraph=False)
@@ -112,26 +86,6 @@ class ss:
         self.button_on = False
 
 def main():
-
-    # window = tk.Tk()
-
-    # The button configuration
-    # button1 = tk.Button(
-    #     text="listener is disabled",
-    #     width=25,
-    #     height=5,
-    #     bg="red",
-    #     fg="white",
-    #     command=enable_mouse_listener
-    # )
-
-
-    # This adds the button to the gui window
-    # button1.pack()
-
-    # The main event loop for the window. Listens for events like button clicks etc
-    # window.mainloop()
-
     ss()
 
 if __name__ == '__main__':
