24e0ae0549654cca10867e34aa7ef05a0ef41864
alex logorz <alex.logorz@gmail.com>
2022-07-01 19:29:14 -0400

    mvp complete. everything except for a small bug when i open and close the canvas without taking screenshot

--- dip.py
+++ dip.py
@@ -1,5 +1,18 @@
 import cv2
+from cv2 import scaleAdd
 import easyocr
+import enum
+import re
+
+class Algorithms(enum.Enum):
+        BINARIZE = 0
+        DB_THRES = 1
+        NONE = 2
+
+def isInRange(array, index):
+        if 0 <= index <= len(array) - 1:
+            return True
+        return False
 
 class ImageProcessor:
     def __init__(self, srcPath, destPath):
@@ -13,12 +26,12 @@ class ImageProcessor:
         image = cv2.threshold(gray_img, threshold, 255, cv2.THRESH_BINARY)[1]
         return image
 
-    def doubleThreshold(self):
+    def doubleThres(self):
         image = cv2.imread(self.srcPath)
         for row in range(0, image.shape[0]):
             for col in range(0, image.shape[1]):
                 pixel = image[row,col]
-                if (188 <= pixel[0] <= 255 and 188 <= pixel[1] <= 255 and 188 <= pixel[2] <= 255) or (0 <= pixel[0] <= 1 and 56 <= pixel[1] <= 145 and 0 <= pixel[2] <= 1) or (93 <= pixel[0] <= 249 and 19 <= pixel[1] <= 50 and 19 <= pixel[2] <= 50):
+                if (180 <= pixel[0] <= 255 and 180 <= pixel[1] <= 255 and 180 <= pixel[2] <= 255) or (pixel[0] == 0 and 126 <= pixel[1] <= 153 and pixel[2] == 0) or (210 <= pixel[0] <= 255 and 42 <= pixel[1] <= 51 and 42 <= pixel[2] <= 51):
                     image[row,col] = [0, 0, 0]
                 else:
                     image[row,col] = [255, 255, 255]
@@ -30,18 +43,53 @@ class ImageProcessor:
     def image2Text(self):
         result = self.reader.readtext(self.srcPath, detail = 0, paragraph=False)
         return result
+    
+    # overloaded to specify preprocessing
+    def image2Text(self, algo):
+        if(algo == Algorithms.BINARIZE):
+            bin_img = self.binarize(210)
+            self.saveImage(bin_img)
+            result = self.reader.readtext(self.destPath, detail = 0, paragraph=False)
+        elif(algo == Algorithms.DB_THRES):
+            db_thres = self.doubleThres()
+            self.saveImage(db_thres)
+            result = self.reader.readtext(self.destPath, detail = 0, paragraph=False)
+        else:
+            result = self.reader.readtext(self.srcPath, detail = 0, paragraph=False)
+        return result
 
     def extractSalesList(self, results):
         seller = results[0]
         itemNames = []
         itemPrices = []
+        salesList = results[results.index('Sale List') + 1:len(results)]
+        
+        print("SUBARRAY LIST: ", salesList)
+        print("\n")
 
-        for i in range(results.index('Chat') + 1, len(results) - 1):
-            currentItem = results[i]
-            if "," in currentItem or "." in currentItem:
+        for currentItemIdx in range(0, len(salesList)):
+            currentItem = salesList[currentItemIdx]
+            # match item price
+            if re.search("[.,]", currentItem):
                 itemPrices.append(currentItem)
+            # match item name
             else:
-                itemNames.append(currentItem)
+                # an item tier
+                if(re.search("^\+\d{1,2}$", currentItem)):
+                    pass
+                else:
+                    # just a guard to protect against out of range error
+                    if isInRange(salesList, currentItemIdx + 1):
+                        # if next item is a tier then it belongs to this items name
+                        nextItem = salesList[currentItemIdx + 1]
+                        if re.search("^\+\d{1,2}$", nextItem):
+                            itemNames.append(currentItem + " " + nextItem)
+                        else:
+                            itemNames.append(currentItem)
+                    else:
+                        # no need to worry about tiers here since there is no next item
+                        itemNames.append(currentItem)
 
         return [seller, itemNames, itemPrices]
+
 
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from pynput import mouse
 from dip import ImageProcessor
+from dip import Algorithms
+from dip import isInRange
 import tkinter as tk
 import pyautogui
 
@@ -17,8 +19,12 @@ class Utilities:
         self.toggleCanvas()
         pyautogui.screenshot(region=(x0, y0, x1 - x0, y1 - y0)).save(savePath)
         proc = ImageProcessor(savePath, destPath)
-        results = proc.image2Text()
+        results = proc.image2Text(Algorithms.DB_THRES)
+        print("RESULTS: ", results)
+        print("\n")
         salesList = proc.extractSalesList(results)
+        print("SALES LIST: ", salesList)
+        print("\n")
         self.openResultsWindow(salesList)
 
     def setRect(self, event):
@@ -63,29 +69,33 @@ class Utilities:
     def closeCanvasWindow(self):
         self.window.destroy()
 
-    def openResultsWindow(self, results):
-        self.window2 = tk.Tk()
-        self.window2.title("Edit Results")
+    def openResultsWindow(self, salesList):
+        self.window = tk.Tk()
+        self.window.title("Edit Results")
         # labels for the columns
-        tk.Label(self.window2 ,text = "Item Names").grid(row = 0,column = 0)
-        tk.Label(self.window2 ,text = "Item Prices").grid(row = 0,column = 1)
+        tk.Label(self.window ,text = "Item Names").grid(row = 0,column = 0)
+        tk.Label(self.window ,text = "Item Prices").grid(row = 0,column = 1)
         # creates the grid system
-        for i in range(1, len(results[1]) + 1):
+        for itemIdx in range(0, len(salesList[1])):
             # text input for item name
             itemText = tk.StringVar()
-            tk.Entry(self.window2, textvariable=itemText).grid(row = i, column = 0, padx=5, pady=2, ipadx=3, ipady=3)
-            itemText.set(results[1][i - 1])
+            tk.Entry(self.window, textvariable=itemText).grid(row = itemIdx, column = 0, padx=5, pady=2, ipadx=3, ipady=3)
+            itemText.set(salesList[1][itemIdx])
             # text input for item price
             priceText = tk.StringVar()
-            tk.Entry(self.window2, textvariable=priceText).grid(row = i, column = 1, padx=5, pady=2, ipadx=3, ipady=3)
-            priceText.set(results[2][i - 1])
+            tk.Entry(self.window, textvariable=priceText).grid(row = itemIdx, column = 1, padx=5, pady=2, ipadx=3, ipady=3)
+            # fixes an out of range bug
+            if isInRange(salesList[2], itemIdx):
+                priceText.set(salesList[2][itemIdx])
+            else:
+                priceText.set(0)
         # text input for sellers name
         sellerText = tk.StringVar()
-        tk.Entry(self.window2, textvariable=sellerText).grid(row = len(results[1]) + 2, column = 0, padx=5, pady=5)
-        sellerText.set(results[0][0])
+        tk.Entry(self.window, textvariable=sellerText).grid(row = len(salesList[1]) + 1, column = 0, padx=5, pady=5)
+        sellerText.set(salesList[0])
         # submit button
-        tk.Button(self.window2, text ="Looks good!", command = self.submitResultsForm).grid(row = len(results[1]) + 2, column = 1, padx=5, pady=2, ipadx=3, ipady=3)
-        self.window2.mainloop()
+        tk.Button(self.window, text ="Looks good!", command = self.submitResultsForm).grid(row = len(salesList[1]) + 1, column = 1, padx=5, pady=2, ipadx=3, ipady=3)
+        self.window.mainloop()
 
 def main():
     util = Utilities()
